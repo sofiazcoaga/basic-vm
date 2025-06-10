c83a015cc044a245a501851de3d8c14a1732d062
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-10 17:52:24 -0300

    parse binary file and copy to memory

---
+++ src/error.rs
@@ -0,0 +1,3 @@
+pub enum VMError {
+    CouldNotReadFile(String), 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use crate::consts::*;
+use std::fs;
+
+use crate::{consts::*, error::VMError};
 
 mod consts;
+mod error;
 fn main() {
     // Initialize memory
     let memory: [u16; MEMORY_MAX] = [0; MEMORY_MAX];
@@ -45,4 +48,28 @@ fn main() {
 
 fn mem_read(address: u16, memory: &[u16; MEMORY_MAX]) -> u16 {
     memory[address as usize]
+}
+
+fn mem_write(address: u16, val: u16, memory: &mut [u16; MEMORY_MAX]) {
+    memory[address as usize] = val;
+}
+
+fn parse_file_to_memory(path: &str, memory: &mut [u16; MEMORY_MAX]) -> Result<(), VMError>{
+    // The origin tells us where in memory to place the content.
+    let whole_file = fs::read(path).unwrap();
+    //let whole_file = fs::read(path).map_err(|x| Err(VMError::CouldNotReadFile(x.to_string()))); // TODO
+
+    let mut file_index = 0;
+    let origin = u16::from_be_bytes([whole_file[file_index], whole_file[file_index + 1]]);
+    file_index += 2;
+
+    let mut offset = origin;
+    while file_index + 1 < whole_file.len() {
+        let content = u16::from_be_bytes([whole_file[file_index], whole_file[file_index + 1]]);
+        mem_write(offset, content, memory);
+        file_index += 2;
+        offset += 1;
+    }
+
+    Ok(())
 }
