be50a8eacb430be265af6a64ac9596d9e88bcb04
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-11 16:22:09 -0300

    add utils file for operations

--- src/operations/add.rs
+++ src/operations/add.rs
@@ -1,4 +1,8 @@
-use crate::{VMState, error::VMError, flags::Flag, registers::Register};
+use crate::{
+    VMState,
+    error::VMError,
+    operations::utils::{sign_extend, update_flags},
+};
 
 pub fn handle_add(instruction: u16, vm: &mut VMState) -> Result<(), VMError> {
     let dest_reg = ((instruction >> 9) & 0x7) as usize;
@@ -17,27 +21,10 @@ pub fn handle_add(instruction: u16, vm: &mut VMState) -> Result<(), VMError> {
     Ok(())
 }
 
-fn sign_extend(mut number: u16, bit_count: usize) -> u16 {
-    if ((number >> (bit_count - 1)) & 1) > 0 {
-        number |= 0xFFFF << bit_count;
-    }
-    number
-}
-
-fn update_flags(vm: &mut VMState, register_value: u16) -> Result<(), VMError> {
-    let cond_register = &mut vm.registers[Register::Cond.usize()];
-    if register_value == 0 {
-        *cond_register = Flag::Zro.try_into()?;
-    } else if (register_value >> 15) > 0 {
-        *cond_register = Flag::Neg.try_into()?;
-    } else {
-        *cond_register = Flag::Pos.try_into()?;
-    }
-    Ok(())
-}
-
 #[cfg(test)]
 mod test {
+
+    use crate::{flags::Flag, registers::Register};
 
     use super::*;
 
