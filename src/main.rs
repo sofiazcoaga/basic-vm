fdf2c93fca9f3a2193aa7587ae1e0a31626a0bc1
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-11 12:20:30 -0300

    change registers and opcodes from constants to enums

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,54 @@
-use crate::consts::*;
+use crate::error::VMError;
+use crate::flags::Flag;
+use crate::opcodes::Opcode::{self, *};
+use crate::registers::Register::{self, *};
+mod error;
+mod flags;
+mod opcodes;
+mod registers;
 
-mod consts;
-fn main() {
+pub const MEMORY_MAX: usize = 1 << 16;
+fn main() -> Result<(), VMError> {
     // Initialize memory
     let memory: [u16; MEMORY_MAX] = [0; MEMORY_MAX];
 
     // Fill memory with instructions here
 
     // Initialize registers
-    let mut registers: [u16; REGISTER_COUNT] = [0; REGISTER_COUNT];
-    registers[COND] = FL_ZRO;
-    registers[PC] = 0x3000; // Set PC to starting position. 0x3000 is the default.
+    let mut registers: [u16; Register::COUNT] = [0; Register::COUNT];
+    registers[Cond.usize()] = Flag::Zro.try_into()?;
+    registers[PC.usize()] = 0x3000; // Set PC to starting position. 0x3000 is the default.
 
     let mut running = true;
 
     while running {
         // Get the next instruction
-        let ix: u16 = mem_read(registers[PC], &memory);
-        registers[PC] += 1;
-        let opcode = ix >> 12;
+        let ix: u16 = mem_read(registers[PC.usize()], &memory);
+        registers[PC.usize()] += 1;
+        let opcode = Opcode::try_from(ix >> 12)?;
 
         match opcode {
-            OP_ADD => println!("Opcode is ADD"),
-            OP_AND => println!("Opcode is AND"),
-            OP_NOT => println!("Opcode is NOT"),
-            OP_BR => println!("Opcode is BR"),
-            OP_JMP => println!("Opcode is JMP"),
-            OP_JSR => println!("Opcode is JSR"),
-            OP_LD => println!("Opcode is LD"),
-            OP_LDI => println!("Opcode is LDI"),
-            OP_LDR => println!("Opcode is LDR"),
-            OP_LEA => println!("Opcode is LEA"),
-            OP_ST => println!("Opcode is ST"),
-            OP_STI => println!("Opcode is STI"),
-            OP_STR => println!("Opcode is STR"),
-            OP_TRAP => println!("Opcode is TRAP"),
-            OP_RES => println!("Opcode is RES"),
-            OP_RTI => println!("Opcode is RTI"),
-            _ => println!("Bad opcode"),
+            OpADD => println!("Opcode is ADD"),
+            OpAND => println!("Opcode is AND"),
+            OpNOT => println!("Opcode is NOT"),
+            OpBR => println!("Opcode is BR"),
+            OpJMP => println!("Opcode is JMP"),
+            OpJSR => println!("Opcode is JSR"),
+            OpLD => println!("Opcode is LD"),
+            OpLDI => println!("Opcode is LDI"),
+            OpLDR => println!("Opcode is LDR"),
+            OpLEA => println!("Opcode is LEA"),
+            OpST => println!("Opcode is ST"),
+            OpSTI => println!("Opcode is STI"),
+            OpSTR => println!("Opcode is STR"),
+            OpTRAP => println!("Opcode is TRAP"),
+            OpRES => println!("Opcode is RES"),
+            OpRTI => println!("Opcode is RTI"),
         }
         running = false; // Temporarily until opcodes are filled.
     }
+
+    Ok(())
 }
 
 fn mem_read(address: u16, memory: &[u16; MEMORY_MAX]) -> u16 {
