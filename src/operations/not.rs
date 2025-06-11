97480cde551993a99e1027fdb22060818521018f
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-11 16:34:21 -0300

    add not test

--- src/operations/not.rs
+++ src/operations/not.rs
@@ -6,3 +6,23 @@ pub fn handle_not(instruction: u16, vm: &mut VMState) -> Result<(), VMError> {
     vm.registers[dest_reg] = !vm.registers[src_reg];
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{registers::Register, VMState};
+    use super::*;
+
+    #[test]
+    fn inverts_register_with_not() {
+        let mut vm = VMState::init().unwrap();
+        // NOT  R1  R1  FILL
+        // 1001 001 001 111111
+        let not_ix = 0x927F;
+        vm.registers[Register::R1.usize()] = 0;
+        let res = handle_not(not_ix, &mut vm);
+        assert!(res.is_ok());
+        //  0 - 0000 0000 0000 0000
+        // !0 - 1111 1111 1111 1111 - MAX VALUE 2^16 - 1 = 65.535
+        assert_eq!(vm.registers[Register::R1.usize()], 65535);
+    }
+}
