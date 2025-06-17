51c1a3ce89eff2703a0031629c9bd94f482f1fd3
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-17 11:19:07 -0300

    add fix of file reading from le bytes to be bytes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::{self};
 
+use termion::raw::IntoRawMode;
+
 use crate::error::VMError;
 
 mod error;
@@ -46,8 +48,8 @@ impl VMState {
 
 fn main() -> Result<(), VMError> {
     // Fill memory with instructions here
-    let example_file = read_file("./binary-examples/2048.obj")?;
-
+    let example_file = read_file("./binary-examples/rogue.obj")?;
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
     // Initialize VM state
     let mut vm = VMState::init()?;
 
@@ -105,7 +107,7 @@ fn write_ixs_to_mem(parsed_file: Vec<u8>, vm: &mut VMState) {
     let mut offset = origin;
     while file_index + 1 < parsed_file.len() {
         // LC3 binaries come in big endian but we need to store it swapped
-        let content = u16::from_le_bytes([parsed_file[file_index], parsed_file[file_index + 1]]);
+        let content = u16::from_be_bytes([parsed_file[file_index], parsed_file[file_index + 1]]);
         mem_write(offset, content, vm);
         file_index += 2;
         offset += 1;
@@ -135,13 +137,7 @@ mod test {
         ]
         .concat();
         write_ixs_to_mem(binary, &mut vm);
-        assert_eq!(
-            vm.memory[origin as usize],
-            u16::from_le_bytes(first_ix.to_be_bytes())
-        );
-        assert_eq!(
-            vm.memory[(origin + 1) as usize],
-            u16::from_le_bytes(second_ix.to_be_bytes())
-        );
+        assert_eq!(vm.memory[origin as usize], first_ix);
+        assert_eq!(vm.memory[(origin + 1) as usize], second_ix);
     }
 }
