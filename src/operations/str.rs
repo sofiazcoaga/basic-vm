bbe998119624ff11afca03d561e7aecc4d9215a4
Sofi Azcoaga <sofiaazc3@hotmail.com>
2025-06-17 10:24:19 -0300

    add test for STR opcode

--- src/operations/str.rs
+++ src/operations/str.rs
@@ -10,3 +10,30 @@ pub fn handle_str(instruction: u16, vm: &mut VMState) -> Result<(), VMError> {
     );
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{VMState, operations::str::handle_str, registers::Register};
+
+    #[test]
+    fn executes_str() {
+        // Setup
+        let mut vm = VMState::init().unwrap();
+        let random_memory_addr = 0x4000;
+        let random_content = 0x1234;
+        let offset = 0x0002;
+        vm.registers[Register::R3.usize()] = random_memory_addr;
+        vm.registers[Register::R1.usize()] = random_content;
+        let calculated_address = (random_memory_addr + offset) as usize;
+
+        assert_eq!(vm.memory[calculated_address], 0); // Still has nothing up to this point.
+
+        // STR  sr_reg (R1) base_reg (R3) offset (2)
+        // 0111 001         011           000010
+        let str_ix = 0x72C2;
+
+        let res = handle_str(str_ix, &mut vm);
+        assert!(res.is_ok());
+        assert_eq!(vm.memory[calculated_address], random_content);
+    }
+}
